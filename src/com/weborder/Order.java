d618527bbabb32b5372f47029cb85ca12a8f0590
MattKara <fbmuratkara@gmail.com>
2019-07-26 08:36:11 -0400

    modify class path and source

--- src/com/weborder/Order.java
+++ src/com/weborder/Order.java
@@ -1,9 +1,22 @@
 package com.weborder;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Order {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/Users/murat/workspace_selenium/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+		
+		driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+		driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+		
+		driver.findElement(By.id("ctl00_MainContent_login_button")).click();
 
 	}
 
